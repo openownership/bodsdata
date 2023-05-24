855d1ba28edbdecc7f9915fc216bd29fdb399013
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-05-24 14:28:06 +0100

    tests/test_stages.py: Add docstrings to tests

--- tests/test_stages.py
+++ tests/test_stages.py
@@ -7,6 +7,7 @@ import json
 import bodsdata
 
 def setup_directories(source, output_dir):
+    """Setup temporary directory structure and copy input data from fixtures"""
     out_dir = Path(output_dir) / source
     out_dir.mkdir(parents=True, exist_ok=True)
     source_dir = Path(output_dir) / f"{source}_download"
@@ -15,10 +16,12 @@ def setup_directories(source, output_dir):
 
 @pytest.fixture(scope="session")
 def output_dir(tmpdir_factory):
-    d = tmpdir_factory.mktemp("tmp-data")
-    return d
+    """Fixture to create temporary directory"""
+    temp_dir = tmpdir_factory.mktemp("tmp-data")
+    return temp_dir
 
 def test_json_zip(output_dir):
+    """Test creation of output json.zip file containing merged JSON Lines from input files"""
     source = 'test-source'
     d = setup_directories(source, output_dir)
     bodsdata.output_dir = output_dir
@@ -29,4 +32,3 @@ def test_json_zip(output_dir):
             print(data)
             assert len(data) == 20
             assert json.loads(data[0].strip())['interestedParty']['describedByPersonStatement'] == '14105856581894595060'
-
