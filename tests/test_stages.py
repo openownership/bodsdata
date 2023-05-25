a18ee61a56a18edeb2e448a1a4deb765e50c4bd8
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-05-25 13:37:03 +0100

    tests: Add test for flatten pipeline stage

--- tests/test_stages.py
+++ tests/test_stages.py
@@ -11,7 +11,7 @@ def setup_directories(source, output_dir):
     out_dir = Path(output_dir) / source
     out_dir.mkdir(parents=True, exist_ok=True)
     source_dir = Path(output_dir) / f"{source}_download"
-    copy_tree('tests/fixtures/gzip', str(source_dir))
+    copy_tree("tests/fixtures/gzip", str(source_dir))
     return out_dir
 
 @pytest.fixture(scope="session")
@@ -20,13 +20,30 @@ def output_dir(tmpdir_factory):
     temp_dir = tmpdir_factory.mktemp("tmp-data")
     return temp_dir
 
+def test_flatten(output_dir):
+    """Test flattening of JSON Lines input files"""
+    source = 'test-source'
+    test_dir = setup_directories(source, output_dir)
+    bodsdata.output_dir = output_dir
+    bodsdata.flatten(source, False)
+    csv_dir = test_dir / "csv"
+    print(list(csv_dir.iterdir()))
+    with open(csv_dir / "ooc_statement.csv") as ooc_statements:
+        data = ooc_statements.readlines()
+        print(data)
+        assert len(data) == 17
+    with open(csv_dir / "person_statement.csv") as person_statements:
+        data = person_statements.readlines()
+        print(data)
+        assert len(data) == 5
+
 def test_json_zip(output_dir):
     """Test creation of output json.zip file containing merged JSON Lines from input files"""
     source = 'test-source'
-    d = setup_directories(source, output_dir)
+    test_dir = setup_directories(source, output_dir)
     bodsdata.output_dir = output_dir
     bodsdata.json_zip(source)
-    with zipfile.ZipFile(d / 'json.zip') as test_zip:
+    with zipfile.ZipFile(test_dir / 'json.zip') as test_zip:
         with test_zip.open(f'{source}.json') as output_file:
             data = output_file.readlines()
             print(data)
