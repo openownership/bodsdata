fb11a4d539858427ecc8f324c8b70d119addcbd1
Ed (ODSC) <ed.lloyd-davies@opendataservices.coop>
2023-06-24 10:48:52 +0100

    consistency_checks.py: Final changes

--- consistency_checks.py
+++ consistency_checks.py
@@ -191,16 +191,14 @@ class ConsistencyChecks:
 
     def _skip_errors(self, stats):
         """Skip any known errors"""
-        print(stats["missing"] > 0, stats["missing"] is self.check_missing_fields)
-        print(stats["duplicate"] > 0, stats["duplicate"] is self.check_statement_dups)
-        print(stats["reference"] > 0, stats["reference"] is self.check_statement_refs, 
-              stats["reference"], type(stats["reference"]), self.check_statement_refs, type(self.check_statement_refs))
-        if stats["missing"] > 0 and not stats["missing"] is self.check_missing_fields:
+        if stats["missing"] > 0 and (isinstance(self.check_missing_fields, bool) or
+                                     stats["missing"] != self.check_missing_fields):
             return False
-        elif stats["duplicate"] > 0 and not stats["duplicate"] is self.check_statement_dups:
+        elif stats["duplicate"] > 0 and (isinstance(self.check_statement_dups, bool) or
+                                         stats["duplicate"] != self.check_statement_dups):
             return False
-        #elif stats["reference"] > 0 and not stats["reference"] is self.check_statement_refs:
-        elif stats["reference"] > 0 and (isinstance(self.check_statement_refs, bool) or stats["reference"] != self.check_statement_refs):
+        elif stats["reference"] > 0 and (isinstance(self.check_statement_refs, bool) or
+                                         stats["reference"] != self.check_statement_refs):
             return False
         else:
             return True
@@ -213,7 +211,6 @@ class ConsistencyChecks:
             output_text(self.console, f"{len(self.error_log)} errors: truncated at {self.error_limit}", "red")
         if len(self.error_log) > 0:
             stats = self._error_stats()
-            print(stats)
             if not self._skip_errors(stats):
                 estats = []
                 for e in stats:
